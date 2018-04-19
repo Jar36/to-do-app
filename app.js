0e23f31a8208b5b212b9a82d06f201d7d77728a7
Joseph Romero <jromero361980@gmail.com>
2018-04-18 19:10:33 -0600

    add styles

--- app.js
+++ app.js
@@ -8,24 +8,28 @@ function onReady(){
 
     let title = newToDoText.value;
     let newLi = document.createElement('li');
+    newLi.classList.add('mdl-list__item');
 
     let checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
+    
+    checkbox.type = 'checkbox';
 
     let button = document.createElement('input');
-    button.type = "button";
+    button.className = 'mdl-button mdl-js-button mdl-button--raised mdl-button--colored';
+    componentHandler.upgradeElement(button);
+    button.type = 'button';
     button.value ='Delete Item';
 
 
     newLi.textContent = title;
-    newLi.appendChild(checkbox);
+    newLi.prepend(checkbox);
     newLi.appendChild(button);
     toDoList.appendChild(newLi);
 
     newToDoText.value = '';
 
     button.addEventListener('click', () => {
-        newLi.remove(this);
+      newLi.remove(this);
     });
 
 
